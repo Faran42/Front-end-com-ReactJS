358b5937ca01a101cc81baae7f85fda7419302bb
Faran42 <faranorphys@gmail.com>
2020-04-20 15:19:30 -0300

    Propriedades

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import React from 'react';
 import Header from './components/Header';
 
 function App() {
-  return<Header />;
+  return(
+    <>
+      <Header title = "Homepage">
+        <ul>
+          <li>Semente</li>
+          <li>Caroço</li>
+          <li>Manga</li>
+        </ul>
+      </Header>
+      <Header title = "Projetos">
+        <ul>
+          <li>Semente</li>
+          <li>Caroço</li>
+          <li>Manga</li>
+        </ul>
+      </Header>
+    </>
+  );  
 }
 
 export default App;
