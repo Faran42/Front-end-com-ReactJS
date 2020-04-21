eab65254b45fda180ed7c7215a2f706c1b931413
Faran42 <faranorphys@gmail.com>
2020-04-21 19:37:45 -0300

    Import .css and img files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import './App.css'
+import backgroundImage from './assets/background.jpeg'
 
 import Header from './components/Header';
 
 function App() {
+  const [projects, setProjects] = useState(['Desenvolvimento de app','Front-end web']);
+
+  // useState retorna um array com 2 posições
+  // 1. Variável com o seu valor inicial
+  // 2. Função para atualizarmos esse valor
+ 
+  function handleAddProject(){
+    //projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+
+    console.log(projects);
+  }
   return(
     <>
-      <Header title = "Homepage">
-        <ul>
-          <li>Semente</li>
-          <li>Caroço</li>
-          <li>Manga</li>
-        </ul>
-      </Header>
-      <Header title = "Projetos">
-        <ul>
-          <li>Semente</li>
-          <li>Caroço</li>
-          <li>Manga</li>
-        </ul>
-      </Header>
+      <Header title = "Projects"/>
+
+      <img width={300} src={backgroundImage}/> <br/>
+
+      <button type="button" onClick={handleAddProject}>Adcionar projeto</button>
+      <ul>
+        {projects.map(project => <li key={project}>{project}</li>)}
+      </ul>
+
+     
     </>
   );  
 }
